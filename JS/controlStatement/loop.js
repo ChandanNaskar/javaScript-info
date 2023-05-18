696bf910d5a899782d29f0b1ab4285632e666d9e
chandan <chandan.naskar.ph@gmail.com>
2023-05-18 19:16:43 +0530

    loop update

---
+++ JS/controlStatement/loop.js
@@ -0,0 +1,45 @@
+/*
+A loop in JavaScript is a statement block that repeatedly executes a statement or a series of statements,
+while a particular condition is true or until a specific condition becomes true.
+
+In other words, loop statement is a control flow statement that executes a block of statements
+repeatedly until a termination condition met.
+
+In brief, a loop statement executes the same block of statements repeatedly.
+
+The block of statements can execute many times, from zero to infinite number depending on the requirement.
+Each execution of the loop is called iteration in JavaScript.
+*/
+
+// Loop Control Structure in JavaScript
+/*
+A block of statements executes sequentially in the loop until a specific condition for
+the termination of the loop met. Therefore, a loop control structure comprises two parts:
+
+        * Control statement
+        * Body of the loop
+
+The work of control statement is to evaluate (i.e. test) a certain condition and
+then perform the repeated execution of statements in the block of loop.
+
+We can classify a control structure into two types depending on
+the position of control statement in the loop. They are:
+
+        * Entry-controlled loop
+        * Exit-controlled loop
+*/
+
+// Basic Steps to Process Loops in JavaScript
+/*
+    * Setting and initialization of a counter.
+    * Execution of statements sequentially in the body/block of loop.
+    * Evaluate for a specified condition for the execution of loop.
+    * Incrementing the counter.
+*/
+
+// Types of Loops in JavaScript
+/*
+    * Simple loops
+    * Nested loops
+    * Infinite loops
+*/
