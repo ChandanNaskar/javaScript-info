d854bc1320b16e42879f92bd1fedce3042c209c2
chandan <chandan.naskar.ph@gmail.com>
2023-05-25 13:14:09 +0530

    update

--- JS/dom/getElementById.js
+++ JS/dom/getElementById.js
@@ -1,2 +1,7 @@
+// using variable
 let msg = document.getElementById("msg");
 msg.innerHTML = "Hello, this is CodeMan!";
+
+
+// // single line (not use variable)
+// document.getElementById("msg").innerHTML = "change Paragraph!";
